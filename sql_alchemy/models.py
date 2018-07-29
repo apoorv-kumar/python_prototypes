30c795749800ed10bdffa72d547e134cdd08eb7f
Apoorv Kumar <apoorv.kumar247@gmail.com>
2018-07-30 00:01:35 +0530

    add oncascade behavior + efficient delete mechanism

--- sql_alchemy/models.py
+++ sql_alchemy/models.py
@@ -4,16 +4,23 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+'''
+cascade='all' is handled at ORM level
+ondelete='CASCADE' is handled at DB level
+'''
+
 class Phone(Base):
     __tablename__ = 'phone'
     ptype = Column(String, primary_key=True)
     number = Column(String, primary_key=True)
-    person_id = Column(Integer, ForeignKey('person.id'), primary_key=True)
+    # ON DELETE CASCADE - to make sure phones are deleted for a person.
+    person_id = Column(Integer, ForeignKey('person.id', ondelete='CASCADE'), primary_key=True)
 
 class Person(Base):
     __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
     age = Column(Integer)
     name = Column(String)
+    # cascade
     phones = relationship("Phone", cascade="all, delete-orphan")
 
--- sql_alchemy/write_to_pg_async.py
+++ sql_alchemy/write_to_pg_async.py
@@ -8,8 +8,10 @@ engine = create_engine(url)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-Base.metadata.create_all(engine)
-
+# Base.metadata.create_all(engine)
+# print("done with createall")
+session.query(Person).delete(synchronize_session=False)  # cascades
+print("deleted initial contents.")
 p1 = Person(
     id=1,
     name="Apoorv",
@@ -23,20 +25,27 @@ p2 = Person(
     age=26,
     phones=[Phone(ptype='home', number=444), Phone(ptype='office', number=555)])
 
-session.add_all([p1, p2])
+p3 = Person(
+    id=3,
+    name="John",
+    age=26,
+    phones=[Phone(ptype='home', number=888), Phone(ptype='office', number=889)])
+
+
+session.add_all([p1, p2, p3])
 session.commit()
+print("reinstantiated values")
+# -------- BASE STATE ---------
+
 
 session = Session()
-persons = session.query(Person).filter(Person.id.in_([1,2]))
-# inefficient row by row delete (but doesn't need db level cascade)
-for person in persons:
-    session.delete(person)
-
+print("updating new values")
+session.query(Person).filter(Person.id.in_([p1.id, p2.id])).delete(synchronize_session=False)
 p1 = Person(
     id=1,
     name="ApoorvN",
     age=26,
-    phones=[ Phone(ptype='home', number=123), Phone(ptype='office', number=234)])
+    phones=[ Phone(ptype='home', number=124), Phone(ptype='office', number=235)])
 
 
 p2 = Person(
@@ -48,3 +57,4 @@ p2 = Person(
 session.add_all([p1, p2])
 
 session.commit()
+print("updated")
